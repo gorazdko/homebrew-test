a0f7257df51c9ff6c24a31b20411fb823b30eb6b
Gorazd Kovacic <gorazdko@gmail.com>
2021-04-19 22:56:26 +0200

    Create testcask.rb

---
+++ Casks/testcask.rb
@@ -0,0 +1,9 @@
+cask "testcask" do
+  version ""
+  sha256 :no_check
+
+  url "https://github.com/gorazdko/homebrew-test2/files/6338995/seedtool.zip"
+  name "seedtool"
+  desc "seedtool"
+  homepage "https://github.com/gorazdko/homebrew-test2"
+end
