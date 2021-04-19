40dca3ed2592df32059d88ae26853e220b76b19a
Gorazd Kovacic <gorazdko@gmail.com>
2021-04-19 23:24:29 +0200

    Update testcask.rb

--- Casks/testcask.rb
+++ Casks/testcask.rb
@@ -2,7 +2,7 @@ cask "testcask" do
   version "v1.0"
   sha256 :no_check
 
-  url "https://github.com/gorazdko/homebrew-test2/files/6338995/seedtool.zip"
+  url "https://github.com/gorazdko/homebrew-test2/files/6339179/seedtool.app.zip"
   name "seedtool"
   desc "seedtool"
   homepage "https://github.com/gorazdko/homebrew-test2"
