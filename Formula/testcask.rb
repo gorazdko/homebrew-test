5bdd527130e5501e3e1f3ff04fa72791d5acf39d
Gorazd Kovacic <gorazdko@gmail.com>
2021-04-19 22:57:08 +0200

    Delete testcask.rb

--- Formula/testcask.rb
+++
@@ -1,9 +0,0 @@
-cask "testcask" do
-  version ""
-  sha256 :no_check
-
-  url "https://github.com/gorazdko/homebrew-test2/files/6338995/seedtool.zip"
-  name "seedtool"
-  desc "seedtool"
-  homepage "https://github.com/gorazdko/homebrew-test2"
-end
